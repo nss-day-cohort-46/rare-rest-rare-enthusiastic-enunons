20eb4ac647edbfbfe38ce9e777575c55ba338a19
blusanders <blusanders@yahoo.com>
2021-05-17 15:59:26 -0500

    Made migrations

--- rareapi/models/subscription.py
+++ rareapi/models/subscription.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Subscription(models.Model):
-    follower = models.ForeignKey("RareUser", on_delete=models.CASCADE)
+    follower = models.ForeignKey("RareUser", on_delete=models.CASCADE, related_name="subscriptions")
     author = models.ForeignKey("RareUser", on_delete=models.CASCADE)
     created_on = models.DateField(auto_now_add=False, auto_now=True)
     ended_on = models.DateField(auto_now_add=False, auto_now=False)
