49efc0e328edb63f588210f8280f7998573f902d
emilycentko <emily.centko@gmail.com>
2021-05-18 13:22:36 -0500

    Post view for create

--- rareapi/views/post.py
+++ rareapi/views/post.py
@@ -3,10 +3,30 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
-from rareapi.models import Post
+from rareapi.models import Post, RareUser
 
 
 class PostView(ViewSet):
+
+    def create(self, request):
+
+        rare_user = RareUser.objects.get(user=request.auth.user)
+
+        post = Game()
+        game.name = request.data["name"]
+        game.difficulty = request.data["difficulty"]
+        game.number_of_players = request.data["numberOfPlayers"]
+
+        game_type = GameType.objects.get(pk=request.data["gameTypeId"])
+        game.game_type = game_type
+
+        try:
+            game.save()
+            serializer = GameSerializer(game, context={'request': request})
+            return Response(serializer.data)
+
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
 
     def retrieve(self, request, pk=None):
        
