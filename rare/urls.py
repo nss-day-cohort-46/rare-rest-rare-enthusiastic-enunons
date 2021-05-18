bc5a8f761540c3d0487346f54f2586556e10fdc0
emilycentko <emily.centko@gmail.com>
2021-05-18 15:20:51 -0500

    Added fixtures

--- rare/urls.py
+++ rare/urls.py
@@ -1,12 +1,10 @@
+from rest_framework import routers
 from django.contrib import admin
 from django.urls import path
-from rest_framework import routers
-from rareapi.views import PostView
-
-router = routers.DefaultRouter(trailing_slash=False)
-router.register(r'posts', PostView, 'post')
 from django.conf.urls import include
 from rareapi.views import register_user, login_user
+
+router = routers.DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
     path('', include(router.urls)),
