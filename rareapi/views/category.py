1c54b6b5daa225a2d0882697281ed0722c24c484
blusanders <blusanders@yahoo.com>
2021-05-19 13:32:42 -0500

    Category updates

--- rareapi/views/category.py
+++ rareapi/views/category.py
@@ -8,6 +8,19 @@ from rareapi.models import Category, category
 
 class CategoryView(ViewSet):
 
+# ****************************************************
+# get all
+# ****************************************************
+    def list(self, request):
+        categories =  Category.objects.all()
+        serializer = CategorySerializer(categories, many=True, context={'request': request})
+        return Response(serializer.data)
+
+
+# ****************************************************
+# get one
+# ****************************************************
+
     def retrieve(self, request, pk=None):
         try:
             category = Category.objects.get(pk=pk)
@@ -17,10 +30,10 @@ class CategoryView(ViewSet):
         except Exception as ex:
             return HttpResponseServerError(ex)
 
-    def list(self, request):
-        categories =  Category.objects.all()
-        serializer = CategorySerializer(categories, many=True, context={'request': request})
-        return Response(serializer.data)
+
+# ****************************************************
+# create
+# ****************************************************
 
     def create(self, request):
         """Handle POST operations
@@ -40,6 +53,11 @@ class CategoryView(ViewSet):
         except ValidationError as ex:
             return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
 
+
+# ****************************************************
+# update
+# ****************************************************
+
     def update(self, request, pk=None):
         """Handle PUT requests for a game
 
@@ -51,6 +69,11 @@ class CategoryView(ViewSet):
         category.save()
 
         return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+
+# ****************************************************
+# destroy
+# ****************************************************
 
     def destroy(self, request, pk=None):
             """Handle DELETE requests for a single game
@@ -70,6 +93,10 @@ class CategoryView(ViewSet):
             except Exception as ex:
                 return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+
+# ****************************************************
+# serialize
+# ****************************************************
 
 class CategorySerializer(serializers.ModelSerializer):
     """JSON serializer for categories
